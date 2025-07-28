a880a90d041a66c1cdfa7084e9eef3928da8a918
MGE <marketing@maitriglobaleducation.com>
2025-07-28 10:05:29 +0530

    fix

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -7,29 +7,7 @@ import institute from "../schemas/institute";
 import photo from "../schemas/photo";
 import subArea from "../schemas/subArea";
 import video from "../schemas/video";
-import blog from '../schemas/blog'
-import blogType from '../schemas/blogType'
-import speaker from '../schemas/speaker'
-import form from '../schemas/form'
-import scholarship from '../schemas/scholarship'
-import webinar from '../schemas/webinar'
-import event from '../schemas/event'
 
-export const schemaTypes = [
-  blog,
-  blogType,
-  speaker,
-  form,
-  scholarship,
-  webinar,
-  event,
-  institute,
-  campus,
-  courseCategory,
-  course,
-  area,
-  subArea,
-  brand,
-  photo,
-  video
-]
+
+
+export const schemaTypes = [institute,campus,courseCategory,course,area,subArea,brand,photo,video];
